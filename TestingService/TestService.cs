dd22df5ccd2dd70efaed59a42e3f21057c4f23cf
Nirel Yehoyada <Nirelko1@gmail.com>
2019-11-10 02:05:59 +0200

    Added Example Code

---
+++ TestingService/TestService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+
+public class TestSevice : ServiceBase
+{
+    public TestSevice()
+    {
+        ServiceName = "TestService";
+    }
+
+    protected override void OnStart(string[] args)
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+}
---
+++ TestingWebService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TestingWebService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = CreateWebHostBuilder(args);
+
+            if (!Debugger.IsAttached)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+
+                webHost.UseContentRoot(pathToContentRoot).Build().RunAsService();
+            }
+            else
+            {
+                webHost.Build().Run();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:8080");
+    }
+}
